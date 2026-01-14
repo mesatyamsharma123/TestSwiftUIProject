aa0c42a1b78f1fbf534ac5c0bd6b0d8f6701917d
mesatyamsharma123 <mesatyamsharma@gmail.com>
2026-01-14 12:46:25 +0530

    Initial Commit

---
+++ TestSwiftUIProject/TestSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIProjectApp.swift
+//  TestSwiftUIProject
+//
+//  Created by Satyam Sharma Chingari on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestSwiftUIProjectTests/TestSwiftUIProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIProjectTests.swift
+//  TestSwiftUIProjectTests
+//
+//  Created by Satyam Sharma Chingari on 14/01/26.
+//
+
+import Testing
+@testable import TestSwiftUIProject
+
+struct TestSwiftUIProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
